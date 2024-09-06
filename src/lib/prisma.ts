d66f3aefd78439a52259cecd61278e6fdc4cb4ac
brightybaron <ardi.panda54@protonmail.com>
2024-09-06 12:18:45 +0700

    123

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-// export const prisma = globalThis.prisma || new PrismaClient();
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+export const prisma = globalThis.prisma || new PrismaClient();
+if (import.meta.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
