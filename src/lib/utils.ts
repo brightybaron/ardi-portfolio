c403ae735890aa58607fc2519c67711fe6bf0d37
brightybaron <ardi.panda54@protonmail.com>
2024-09-09 13:39:44 +0700

    some changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,13 @@
-export const formatDate = (dateStr: string) => {
+export const formatDate = (dateStr: string, path: string) => {
   const date = new Date(dateStr);
+
+  const isPath =
+    path.includes("/posts") ||
+    path.includes("/projects") ||
+    path.includes("/photos");
+
   const formatter = new Intl.DateTimeFormat("en-US", {
-    month: "long",
+    month: isPath ? "long" : "short",
     day: "numeric",
     year: "numeric",
   });
